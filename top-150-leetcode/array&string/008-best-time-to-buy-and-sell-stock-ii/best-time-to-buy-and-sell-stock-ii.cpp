eedab6db917b2146aa7ffe59b8b29e43c7f02e4e
YoussefSayedDev <youssef.el.sayed.email@gmail.com>
2025-07-08 09:04:12 +0300

    feat: add solution and description for 'Best Time to Buy and Sell Stock II' problem

---
+++ top-150-leetcode/array&string/008-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <climits>
+
+using namespace std;
+
+class Solution
+{
+public:
+  int maxProfit(vector<int> &prices)
+  {
+    int profit = 0;
+
+    for (int i = 1; i < prices.size(); i++)
+    {
+      if (prices[i] > prices[i - 1])
+      {
+        profit += prices[i] - prices[i - 1];
+      }
+    }
+    return profit;
+  }
+};
+
+int main()
+{
+  Solution s;
+
+  vector<int> testNums = {7, 1, 5, 3, 6, 4};
+
+  // output: 7
+
+  int profit = s.maxProfit(testNums);
+
+  cout << profit << endl;
+
+  return 0;
+}
